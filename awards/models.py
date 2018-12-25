5b5f6492f0ffd274b7a834be193bdf59ceca939d
Kernael92 <kernaelapuko5@gmail.com>
2018-12-26 01:02:24 +0300

    add a class meta to the profile class so as to return the user's username

--- awards/models.py
+++ awards/models.py
@@ -5,10 +5,13 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-    profile_pic = models.ImageField(upload_to = 'profiles/')
+    profile_pic = models.ImageField(upload_to = 'profile_pics/')
     bio = models.CharField(max_length = 254)
     email = models.EmailField()
     phone_number = models.CharField(max_length = 10, blank = True)
+
+    def __str__(self):
+        return self.user.username
 
 class Project(models.Model):
     user_profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
