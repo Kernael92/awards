e717b7ea1778b227ec5468ae78539f4512846f7a
Kernael92 <kernaelapuko5@gmail.com>
2018-12-26 00:23:22 +0300

    create a new profile model

--- awards/models.py
+++ awards/models.py
@@ -5,6 +5,18 @@ class Project(models.Model):
     title = models.CharField(max_length = 30)
     project_image = models.ImageField(upload_to = 'projects/')
     project_description = models.TextField()
-    project_link = models.URLField(max_length = 245, blank = True)
+    project_link = models.URLField(max_length = 245,)
+
+    def __str__(self):
+        return self.title
+class Profile(models.Model):
+    profile_pic = models.ImageField(upload_to = 'profiles/')
+    bio = models.CharField(max_length = 254)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length = 10, blank = True)
+
+
+
 
 
