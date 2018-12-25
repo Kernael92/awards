38e0a0b55460deb7b11d7ab64c759771eed9222a
Kernael92 <kernaelapuko5@gmail.com>
2018-12-25 21:39:55 +0300

    create a project model class

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length = 30)
+    project_image = models.ImageField(upload_to = 'projects/')
+    project_description = models.TextField()
+    project_link = models.URLField(max_length = 245, blank = True)
+
+
