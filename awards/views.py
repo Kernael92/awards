2834274151acef22ffb98caccc161a5e79e6ca82
Kernael92 <kernaelapuko5@gmail.com>
2018-12-26 18:26:03 +0300

    define the index view function and return the index template page

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Project,Profile
+from django.contrib.auth.models import User
 
 # Create your views here.
+def index(request):
+    projects = Project.objects.all()
+    return render(request, "awards/index.html",{"projects":projects})
