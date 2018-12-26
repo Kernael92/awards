72f52aff2882c0a7dcb705711443023c2244ffbb
Kernael92 <kernaelapuko5@gmail.com>
2018-12-26 19:06:35 +0300

    create an index html to display our projects details

--- kernel/urls.py
+++ kernel/urls.py
@@ -19,6 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.simple.urls')),
-    
-    # url(r'^awards/',include('awards.urls')),
+    url(r'',include('awards.urls')),
 ]
