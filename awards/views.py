d3e11c29d030881bdc1eb086aea6e6a0bf0eee45
Kernael92 <kernaelapuko5@gmail.com>
2018-12-29 23:51:30 +0300

    define a new view function that calls the new project form

--- awards/views.py
+++ awards/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render,redirect
 from .models import Project,Profile
 from django.contrib.auth.models import User
+from .forms import NewProjectForm
+from django.contrib.auth.decorators import login_required.
 
 # Create your views here.
 def index(request):
     projects = Project.objects.all()
     return render(request, "awards/index.html",{"projects":projects})
+@login_required
+def new_project(request):
+    current_user = request.User 
+    if request.metod == 'POST':
+        form = NewProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit = False)
+            project.user_profile = current_user
+            project.save()
+        return redirect ('/')
+    else:
+        form = NewProjectForm()
+    return render(request, 'new_project.html', {'form': form})
+
