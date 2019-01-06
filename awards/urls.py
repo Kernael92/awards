b8d7fe60d271e257868953fcb05602fd6f22e5b1
Kernael92 <kernaelapuko5@gmail.com>
2019-01-06 22:22:23 +0300

    debug the profile setting view function

--- awards/urls.py
+++ awards/urls.py
@@ -6,10 +6,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     url('^$',views.index, name="index"),
     url(r'^new/project$', views.new_project, name='new-project'),
-    url(r'^setting/(?P<username>[\w.@+-]+)/$', views.profile_setting, name='profile_setting'),
+    url(r'^setting/', views.profile_setting, name='profile_setting'),
     url(r'^profile/(?P<username>[\w.@+-]+)/$', views.profile, name='profile'),
     url(r'^search/',views.search_projects, name="search_projects"),
-    url(r'project(\d+)',views.project, name='project')
+    url(r'project(\d+)',views.project, name='project'),
 
 ]
 if settings.DEBUG:
--- awards/views.py
+++ awards/views.py
@@ -14,7 +14,7 @@ def index(request):
 def new_project(request):
     current_user = request.user 
     if request.method == 'POST':
-        form = NewProjectForm(request.POST, request.FILES)
+        form = NewProjectForm(request.POST, request.FILES,)
         if form.is_valid():
             project = form.save(commit = False)
             project.user = current_user
@@ -24,24 +24,23 @@ def new_project(request):
         form = NewProjectForm()
     return render(request, 'new_project.html', {'form': form})
 @login_required
-def profile_setting(request,username):
-    user = User.objects.get(username=username)
-    if request.user != user:
-        return redirect('index')
+def profile_setting(request,):
+    current_user = request.user 
+   
     if request.method == 'POST':
-        form = ProfileForm(request.POST, request.FILES)
+        form = ProfileForm(request.POST, request.FILES,instance=current_user.profile)
         if form.is_valid():
             profile = form.save(commit = False)
-            profile.user = user
+            profile.user = current_user
             profile.save
-        return redirect (reverse('profile',{'username':user.username}))
+        return redirect('index')
+        
     else:
         form = ProfileForm()
     context = {
-        'user': user,
         'form': form
     }
-    return redirect(request, 'awards/profile_setting.html', context)
+    return render(request, 'awards/profile_setting.html', context)
 def profile(request,username):
     user = User.objects.get(username=username)
     if not user:
@@ -67,6 +66,7 @@ def search_projects(request):
 
 @login_required       
 def project(request, id):
+    
     project = Project.objects.get(pk = id)
     current_user = request.user
     comments = Review.get_comment(Review,id)
@@ -87,16 +87,19 @@ def project(request, id):
             review.usability_rating = usability_rating
             review.content_rating = content_rating
             review.save()
-        else:
-            form = ReviewForm
-        context = {
-            'project': project,
-            'form':form,
-            'comments':comments,
-            'review_list':review_list
+    else:
+        form = ReviewForm
 
-        }
-        return render(request, 'awards/project.html', context)
+        
+    context = {
+        'id': id,
+        'project': project,
+        'form':form,
+        'comments':comments,
+        'review_list':review_list
+
+    }
+    return render(request, 'awards/project.html', context)
 
 
 
